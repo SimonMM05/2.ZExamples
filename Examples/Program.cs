d36e77e58bd89721bb09d7d796ad99ae3de3467f
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-12-05 14:48:31 +0100

    Datatyper mangler bool

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,21 +1,32 @@
-﻿using System;
+﻿/*int x; // Declaration
+x = 3*12; // Instantiering
 
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("John er en skattesvindler");
+int y = 2*4, z = 6*4;
 
-        Console.WriteLine("Indtast dit svar");
-        string userInput = Console.ReadLine();
 
-        if (userInput.ToLower() == "true")
-        {
-            Console.WriteLine("-1000000 i statskassen");
-        }
-        else
-        {
-            Console.WriteLine("Forkert svar");
-        }
-    }
-}
+Console.WriteLine("x is " + x, "y is " + y, "z is " + z);
+
+float f = 5.2f;
+Console.WriteLine((z + f + x)/y);
+
+int e = Convert.ToInt16(f);
+Console.WriteLine("float to int " + e);
+*/
+
+using System.Diagnostics;
+
+string str = "Du er grim" + " \nog din far er grim";
+
+Console.WriteLine(str);
+
+
+/* int x = 4;
+Datatype variable navn = værdi 
+Int er hele tal (-5, 6 8 4637926432)
+float decimal tal
+string er tekst in "" fx "Du er grim"
+
+\n laver en ny string på en ny linje
+
+
+*/
