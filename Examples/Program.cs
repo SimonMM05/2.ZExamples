1803a193dec8d4c4ecbd35a8d66df60f0819f475
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-12-06 09:36:51 +0100

    Ready to work

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,4 +1,5 @@
-﻿/*int x; // Declaration
+﻿/*
+int x; // Declaration
 x = 3*12; // Instantiering
 
 int y = 2*4, z = 6*4;
@@ -18,15 +19,54 @@ using System.Diagnostics;
 string str = "Du er grim" + " \nog din far er grim";
 
 Console.WriteLine(str);
+
+bool erDetTirsdag = false;
+Console.WriteLine("Er det tirsdag? " + erDetTirsdag);
 */
 
-/* int x = 4;
+/*
+int x = 1;
+
+if (x == 4)
+{
+    Console.WriteLine("X is 4");
+}
+else if (x == 1)
+{
+    Console.WriteLine("X is 1");
+}
+
+else if (x == 2)
+{
+    Console.WriteLine("X is 2");
+}
+
+else
+{
+    Console.WriteLine("X is not 1, 2 or 4");
+}
+
+string? input = Console.ReadLine();
+Console.WriteLine("This is the input: \n" + input);
+*/
+
+
+Console.WriteLine("What do you want to multiply 2 with?");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("2 * " + number + " = " + (2*number));
+
+/*
+Datatyper
+int x = 4;
 Datatype variable navn = værdi 
-Int er hele tal (-5, 6 8 4637926432)
+Int er hele tal (-5, 6, 8, 4637926432)
 float decimal tal
 string er tekst in "" fx "Du er grim"
 
 \n laver en ny string på en ny linje
+bool is a boolean - sandt eller falsk værdi
 
+Logics
+if statement --> 
 
 */
